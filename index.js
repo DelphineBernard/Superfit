b84188d16388fffcd68e562f22c8a528eeaeec60
DelphineBernard <del.bernard@gmail.com>
2024-04-01 11:31:31 +0200

    add redis config

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ import router from "./app/router.js";
 import pageNotFound from "./app/middlewares/pageNotFound.js";
 import session from "express-session";
 import saveUserData from "./app/middlewares/saveUserData.js";
-
+import { createClient } from 'redis';
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+const client = createClient({
+    url: process.env.REDIS_URL
+});
+
+client.on('error', (err) => console.log('Redis Client Error', err));
 
 app.use(express.json());
 
@@ -21,7 +27,8 @@ app.use(express.static('./assets'));
 
 app.use(express.urlencoded({extended: true}));
 
-app.use(session({                                                 
+app.use(session({
+    store: redisStore,                                          
     resave: true,
     saveUninitialized: true,
     secret: process.env.SECRET,                                  
